09b6677b56cf0765ba4943d09f6c63fada70a986
Sergey Tangyan <sergeytangyan@gmail.com>
2025-04-19 01:06:16 +0400

    progress!

--- cmd/ccxxd/main.go
+++ cmd/ccxxd/main.go
@@ -1,58 +1,71 @@
 // https://github.com/vim/vim/blob/master/src/xxd/xxd.c
+// https://www.analyticsvidhya.com/blog/2024/06/xxd-command-in-linux/
 package main
 
 import (
-	"bufio"
+	"encoding/hex"
 	"flag"
-	"log"
+	"fmt"
 	"os"
 )
 
+var (
+	postscript bool
+	buffSize   int
+)
+
 func main() {
-	log.SetFlags(0)
-
+	flag.BoolVar(&postscript, "p", false, "postscript")
+	flag.IntVar(&buffSize, "c", 16, "column size")
 	flag.Parse()
-	filepath := flag.Arg(0)
 
+	filepath := flag.Arg(0)
 	f, err := os.Open(filepath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	reader := bufio.NewReader(f)
-	// reader := bufio.NewScanner(f)
-	buf1 := make([]byte, 16)
-	buf2 := make([]byte, 16)
+	buf := make([]byte, buffSize)
 
 	offset := 0
-	for i := 0; i < 16; i++ {
-		b, err := reader.ReadByte()
+	for {
+		n, err := f.Read(buf)
 		if err != nil {
-			if i > 0 {
-				printLine(offset, buf1[0:i], buf2[0:i])
-			}
 			break
 		}
 
-		buf1[i] = b
+		if n < buffSize {
+			buf = buf[0:n]
+		}
 
+		printLine(offset, buf, strippedBuf(buf))
+		offset += n
+	}
+}
+
+func strippedBuf(buf []byte) []byte {
+	newBuf := make([]byte, len(buf))
+
+	for i, b := range buf {
 		if b < 32 || b > 126 {
 			b = 46 // ascii period
 		}
 
-		buf2[i] = b
-
-		if i == 15 {
-			i = -1
-			printLine(offset, buf1, buf2)
-			offset++
-		}
+		newBuf[i] = b
 	}
+
+	return newBuf
 }
 
 func printLine(offset int, bufLeft, bufRight []byte) {
+	// FIX column width!!!
+	// if postscript {
+	// 	fmt.Printf("%-32x", bufLeft)
+	// 	return
+	// }
+
 	// rightpad────────┐
 	//  leftpad─┐      │
-	log.Printf("%06x0: %-32x  %s \n", offset, bufLeft, string(bufRight))
-	// https://www.analyticsvidhya.com/blog/2024/06/xxd-command-in-linux/
+	fmt.Printf("%08x: %-32x  %s \n", offset, bufLeft, string(bufRight))
 }
