1e04d6ac74693e7e69ad5100802c7d60790ee7cf
Sergey Tangyan <sergeytangyan@gmail.com>
2025-04-19 14:18:26 +0400

    first prototype

--- cmd/ccxxd/main.go
+++ cmd/ccxxd/main.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
@@ -12,12 +11,17 @@ import (
 var (
 	postscript bool
 	buffSize   int
+	groupSize  int
+	hexPad     int
 )
 
 func main() {
 	flag.BoolVar(&postscript, "p", false, "postscript")
 	flag.IntVar(&buffSize, "c", 16, "column size")
+	flag.IntVar(&groupSize, "g", 4, "group size")
 	flag.Parse()
+
+	hexPad = buffSize*2 + buffSize*2/groupSize
 
 	filepath := flag.Arg(0)
 	f, err := os.Open(filepath)
@@ -39,12 +43,12 @@ func main() {
 			buf = buf[0:n]
 		}
 
-		printLine(offset, buf, strippedBuf(buf))
+		printLine(offset, buf)
 		offset += n
 	}
 }
 
-func strippedBuf(buf []byte) []byte {
+func toStrippedString(buf []byte) string {
 	newBuf := make([]byte, len(buf))
 
 	for i, b := range buf {
@@ -55,17 +59,37 @@ func strippedBuf(buf []byte) []byte {
 		newBuf[i] = b
 	}
 
-	return newBuf
+	return string(newBuf)
 }
 
-func printLine(offset int, bufLeft, bufRight []byte) {
+func toChunkedString(str string) string {
+	var newStrBuf []rune
+
+	for i, r := range str {
+		if (i+1)%groupSize == 0 {
+			newStrBuf = append(newStrBuf, r, ' ')
+		} else {
+			newStrBuf = append(newStrBuf, r)
+		}
+	}
+
+	return string(newStrBuf)
+}
+
+func printLine(offset int, hexBuffer []byte) {
 	// FIX column width!!!
 	// if postscript {
 	// 	fmt.Printf("%-32x", bufLeft)
 	// 	return
 	// }
 
-	// rightpad────────┐
-	//  leftpad─┐      │
-	fmt.Printf("%08x: %-32x  %s \n", offset, bufLeft, string(bufRight))
+	hexStr := fmt.Sprintf("%x", hexBuffer)
+	chunkedStr := toChunkedString(hexStr)
+
+	fmt.Printf(
+		"%s: %s %s\n",
+		fmt.Sprintf("%08x", offset),             // leftpad
+		fmt.Sprintf("%-*s", hexPad, chunkedStr), // dynamic rightpad
+		toStrippedString(hexBuffer),             // replace non-ascii with '.'
+	)
 }
