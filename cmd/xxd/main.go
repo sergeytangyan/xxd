19a9b99412715b288cc99a944fff379fe5aabe24
Sergey Tangyan <sergeytangyan@gmail.com>
2025-04-21 01:20:43 +0400

    buggy write to file

--- cmd/xxd/main.go
+++ cmd/xxd/main.go
@@ -1,65 +1,102 @@
 // https://codingchallenges.fyi/challenges/challenge-xxd
 // https://github.com/vim/vim/blob/master/src/xxd/xxd.c
+// https://www.mankier.com/1/xxd
 // https://www.analyticsvidhya.com/blog/2024/06/xxd-command-in-linux/
 package main
 
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"slices"
 )
 
-var (
-	// postscript    bool
+type command struct {
+	postscript    bool
 	byteBuffSize  int
 	byteGroupSize int
 	hexPad        int
-)
+	out           io.WriteCloser
+	in            io.ReadCloser
+}
 
 const (
-	DEFAULT_G_SIZE = 2
-	DEFAULT_C_SIZE = 16
+	DEFAULT_G   = 2
+	DEFAULT_C   = 16
+	DEFAULT_P_C = 30
+	MAX_C       = 256
 )
 
 func main() {
-	// postscript = *flag.Bool("p", false, "postscript")
-	flag.IntVar(&byteBuffSize, "c", DEFAULT_C_SIZE, "column size")
-	flag.IntVar(&byteGroupSize, "g", DEFAULT_G_SIZE, "group size")
-	flag.Parse()
+	cmd := parseCmd()
+	xxd(cmd)
+}
 
-	if byteBuffSize <= 0 {
-		byteBuffSize = DEFAULT_C_SIZE
-	}
-	if byteGroupSize < 0 {
-		byteGroupSize = DEFAULT_G_SIZE
-	}
-
-	hexPad = byteBuffSize*2 + byteBuffSize/byteGroupSize
-
-	filepath := flag.Arg(0)
-	f, err := os.Open(filepath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func xxd(cmd *command) {
+	defer cmd.in.Close()
+	defer cmd.out.Close()
 
 	offset := 0
-	buf := make([]byte, byteBuffSize)
+	buf := make([]byte, cmd.byteBuffSize)
 
 	for {
-		n, err := f.Read(buf)
+		n, err := cmd.in.Read(buf)
 		if err != nil {
 			break
 		}
 
-		if n < byteBuffSize {
+		if n < cmd.byteBuffSize {
 			buf = buf[0:n]
 		}
 
-		printLine(offset, buf)
+		printLine(cmd, offset, buf)
 		offset += n
 	}
+}
+
+func parseCmd() *command {
+	cmd := &command{}
+
+	flag.BoolFunc("p", "postscript", func(str string) error {
+		cmd.postscript = true
+		cmd.byteBuffSize = DEFAULT_P_C
+		return nil
+	})
+	flag.IntVar(&(cmd.byteBuffSize), "c", DEFAULT_C, "column size")
+	flag.IntVar(&(cmd.byteGroupSize), "g", DEFAULT_G, "group size")
+	flag.Parse()
+
+	if cmd.byteBuffSize > MAX_C {
+		dieAndDump(fmt.Errorf("invalid number of columns (max. %d)", MAX_C))
+	} else if cmd.byteBuffSize <= 0 {
+		cmd.byteBuffSize = DEFAULT_C
+	}
+
+	if cmd.byteGroupSize < 0 {
+		cmd.byteGroupSize = DEFAULT_G
+	} else if cmd.byteGroupSize == 0 {
+		cmd.byteGroupSize = cmd.byteBuffSize * 2
+	}
+
+	cmd.hexPad = cmd.byteBuffSize*2 + cmd.byteBuffSize/cmd.byteGroupSize
+
+	inputPath := flag.Arg(0)
+	f, err := os.Open(inputPath)
+	dieAndDump(err)
+	cmd.in = f
+
+	outputPath := flag.Arg(1)
+	if outputPath != "" {
+		// TODO: FIX WRITING TO FILE
+		outFile, err := os.OpenFile(outputPath, os.O_CREATE, 0o666)
+		dieAndDump(err)
+		cmd.out = outFile
+	} else {
+		cmd.out = os.Stdout
+	}
+
+	return cmd
 }
 
 func toStrippedString(buf []byte) string {
@@ -76,27 +113,34 @@ func toStrippedString(buf []byte) string {
 	return string(newBuf)
 }
 
-func toChunkedHexString(bytes []byte) string {
+func toChunkedHexString(bytes []byte, chunkSize int) string {
 	var str string
 
-	for b := range slices.Chunk(bytes, byteGroupSize) {
+	for b := range slices.Chunk(bytes, chunkSize) {
 		str += fmt.Sprintf("%x ", b)
 	}
 
 	return str
 }
 
-func printLine(offset int, bytes []byte) {
-	// FIX column width!!!
-	// if postscript {
-	// 	fmt.Printf("%-32x", bufLeft)
-	// 	return
-	// }
+func dieAndDump(err error) {
+	if err != nil {
+		fmt.Printf("xxd: %s\n", err)
+		os.Exit(1)
+	}
+}
 
-	fmt.Printf(
+func printLine(cmd *command, offset int, bytes []byte) {
+	if cmd.postscript {
+		fmt.Fprintf(cmd.out, "%-*x\n", cmd.hexPad, bytes)
+		return
+	}
+
+	fmt.Fprintf(
+		cmd.out,
 		"%08x: %-*s %s\n",
-		offset,                            // leftpadded
-		hexPad, toChunkedHexString(bytes), // rightpaded
+		offset,                                                   // leftpadded
+		cmd.hexPad, toChunkedHexString(bytes, cmd.byteGroupSize), // rightpaded
 		toStrippedString(bytes), // replace non-ascii with '.'
 	)
 }
